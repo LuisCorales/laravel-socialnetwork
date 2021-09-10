25bcf87a4a7a628c1d01184d03d6308b48426428
Luis Corales <luisaugcorales@gmail.com>
2021-09-10 16:21:56 -0500

    Add profile table

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -42,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Table relationships
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,9 @@
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>120</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">Somos DevNiors!</div>
-            <div>🎯 | Amigos desarrolladores;<br>💡 | Tips, anécdotas y variedades de #programacion;<br>💻 | Codear es nuestro camino ninja;</div>
-            <div><a href="#">google.com</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
