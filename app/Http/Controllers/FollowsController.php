aa5a3d0aacaee32ae3febbfe594bd5936418fcff
Luis Corales <luisaugcorales@gmail.com>
2021-09-14 13:36:03 -0500

    Following and followers added

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class FollowsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(User $user)
     {
-        return $user->username;
+        $userAuth = Auth::user();
+        return $userAuth->following()->toggle($user->profile);
     }
 }
