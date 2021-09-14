b2c39fde3069ebdbfe5e25b16e23770ea17be77a
Luis Corales <luisaugcorales@gmail.com>
2021-09-14 16:07:02 -0500

    More seeders!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProfileSeeder::class,
             PostSeeder::class,
+            FollowsSeeder::class,
         ]);
     }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -14,12 +14,18 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=0; $i < 5; $i++) {
+        for ($i=0; $i < 12; $i++) {
             DB::table('posts')->insert([
                 'user_id' => '1',
                 'caption' => "This is my post #{{ $i }}!",
-                'image' => '',
+                'image' => 'uploads/2B9Z7a9JSDvE7e9jaDKiPLBKadT92Rl0XMV48EdV.png',
             ]);
         }
+
+        DB::table('posts')->insert([
+            'user_id' => '2',
+            'caption' => "This is my test post!",
+            'image' => 'uploads/2B9Z7a9JSDvE7e9jaDKiPLBKadT92Rl0XMV48EdV.png',
+        ]);
     }
 }
